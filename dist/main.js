893191ebf11ebddad23ae0a0513e53911defd804
danielfdez17 <daniel17dev@gmail.com>
2026-02-27 16:56:33 +0100

    feat: NestJS application starts

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
+    process.env.MONGO_URI ??= `mongodb://${process.env.MONGO_USER ?? 'admin'}:${process.env.MONGO_PASSWORD ?? 'rootpassword'}@${process.env.MONGO_HOST ?? 'localhost'}:${process.env.MONGO_PORT ?? '27018'}`;
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     await app.listen(process.env.PORT ?? 3000);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  process.env.MONGO_URI ??= `mongodb://${process.env.MONGO_USER ?? 'admin'}:${process.env.MONGO_PASSWORD ?? 'rootpassword'}@${process.env.MONGO_HOST ?? 'localhost'}:${process.env.MONGO_PORT ?? '27018'}`;
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
