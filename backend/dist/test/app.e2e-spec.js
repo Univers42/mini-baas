d8ce7b129af7f5bd44e6ab757455b3abc220ad01
vjan-nie <vjan-nie@student.42madrid>
2026-03-11 12:35:04 +0100

    feat(fase0-structure): Cleansing of old structure and files, updating headers and paths according to new project, checking basic dependencies, rewritting Makefile and ci.yml file for git workflow

---
+++ backend/dist/test/app.e2e-spec.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const testing_1 = require("@nestjs/testing");
+const supertest_1 = __importDefault(require("supertest"));
+const app_module_1 = require("../src/app.module");
+describe('AppController (e2e)', () => {
+    let app;
+    beforeEach(async () => {
+        const moduleFixture = await testing_1.Test.createTestingModule({
+            imports: [app_module_1.AppModule],
+        }).compile();
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+    afterAll(async () => {
+        await app.close();
+    });
+    it('/ (GET) should be defined', () => {
+        return (0, supertest_1.default)(app.getHttpServer()).get('/api').expect(404);
+    });
+});
